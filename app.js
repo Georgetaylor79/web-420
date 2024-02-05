251b71b37ab1f87d3378b6e181da30789c3bbc33
George <george_rtaylor@yahoo.com>
2024-02-04 23:07:09 -0600

    commit

---
+++ app.js
@@ -0,0 +1,52 @@
+/*
+============================================
+ Title: macias-composer.js
+ Author: Andres Macias
+ Date:   9/4/22
+ Description: contains schema for composer
+===========================================
+*/
+
+
+let express = require("express");
+let http = require("http");
+let swaggerUi = require("swagger-ui-express");
+let swaggerJsdoc = require("swagger-jsdoc");
+let mongoose = require("mongoose");
+// let composerAPI = require("./routes/macias-composer-routes");
+let teamAPI = require("./routes/taylor-composer-routes");
+// let personAPI = require("./routes/macias-person-routes");
+// let userAPI = require("./routes/macias-session-routes");
+// let customerAPI = require("./routes/macias-node-shopper-routes");
+let app = express();
+let bodyParser = require("body-parser");
+
+
+app.set('port', process.env.PORT || 3000);
+app.set(express.json());
+// app.set(express.urlencoded({'extended': true}));
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use(bodyParser.json()); //this is needed to have swagger pass the json to the node api
+
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        explorer: true,
+        info: {
+            title: 'WEB 420 RESTful APIs',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*js']
+};
+
+let openapiSpecification = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+app.use('/api', teamAPI);
+
+
+
+
+http.createServer(console.log("Application started and listening to port 3000"), app).listen(process.env.PORT || 3000);

96c3db0bc98a200ab9fa7d24290e73287a4f436c
George <george_rtaylor@yahoo.com>
2024-02-04 20:57:05 -0600

    commit

---
+++ routes/taylor-composer-routes.js
@@ -0,0 +1,22 @@
+/*
+============================================
+ Title: taylor-composer-routes.js
+ Author: George Taylor
+ Date:   2/04/2024
+ Description: contains schema for composer
+===========================================
+*/
+
+
+
+
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let composerSchema = new Schema({
+    firstName: { type: String},
+    lastName: {type: String}
+});
+
+module.exports = mongoose.model('Composer', composerSchema);
